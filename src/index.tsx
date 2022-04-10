f564946beb96984ebf82a86712356ed869a8276d
XPerezX <perez@startapp.one>
2022-04-10 13:27:42 -0300

    cleaning some useless files and adding font

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./pages/App";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorker";
 import theme from "./theme";
 
 import { ChakraProvider } from "@chakra-ui/react";
@@ -20,11 +19,6 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
